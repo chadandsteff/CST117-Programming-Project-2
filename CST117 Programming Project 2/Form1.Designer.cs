b8dbc146c72aca887210f80885c338f4acbbf462
chadandsteff <chad@chadandstephanieweirick.com>
2018-11-30 19:18:40 -0700

    CST 117 programming project 2
    CST117_OC2

--- CST117 Programming Project 2/Form1.Designer.cs
+++ CST117 Programming Project 2/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CST117_Programming_Project_2
 {
-    partial class Form1
+    partial class passGenMainWindow
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.includeNumbersCheckBox = new System.Windows.Forms.CheckBox();
+            this.includeSpecialCharactersCheckBox = new System.Windows.Forms.CheckBox();
+            this.includeUpperAndLowerCaseCheckBox = new System.Windows.Forms.CheckBox();
+            this.resultsListBox = new System.Windows.Forms.ListBox();
+            this.generateOnePassRadioButton = new System.Windows.Forms.RadioButton();
+            this.generateFivePasswordsRadioButton = new System.Windows.Forms.RadioButton();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.generateRadioButtonContainer = new System.Windows.Forms.GroupBox();
+            this.copyButton = new System.Windows.Forms.Button();
+            this.generateRadioButtonContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // includeNumbersCheckBox
+            // 
+            this.includeNumbersCheckBox.AutoSize = true;
+            this.includeNumbersCheckBox.Location = new System.Drawing.Point(12, 12);
+            this.includeNumbersCheckBox.Name = "includeNumbersCheckBox";
+            this.includeNumbersCheckBox.Size = new System.Drawing.Size(128, 17);
+            this.includeNumbersCheckBox.TabIndex = 1;
+            this.includeNumbersCheckBox.Text = "Include numbers (0-9)";
+            this.includeNumbersCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // includeSpecialCharactersCheckBox
+            // 
+            this.includeSpecialCharactersCheckBox.AutoSize = true;
+            this.includeSpecialCharactersCheckBox.Location = new System.Drawing.Point(13, 36);
+            this.includeSpecialCharactersCheckBox.Name = "includeSpecialCharactersCheckBox";
+            this.includeSpecialCharactersCheckBox.Size = new System.Drawing.Size(210, 17);
+            this.includeSpecialCharactersCheckBox.TabIndex = 2;
+            this.includeSpecialCharactersCheckBox.Text = "Include special characters (!@#$%^&*())";
+            this.includeSpecialCharactersCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // includeUpperAndLowerCaseCheckBox
+            // 
+            this.includeUpperAndLowerCaseCheckBox.AutoSize = true;
+            this.includeUpperAndLowerCaseCheckBox.Location = new System.Drawing.Point(12, 60);
+            this.includeUpperAndLowerCaseCheckBox.Name = "includeUpperAndLowerCaseCheckBox";
+            this.includeUpperAndLowerCaseCheckBox.Size = new System.Drawing.Size(169, 17);
+            this.includeUpperAndLowerCaseCheckBox.TabIndex = 3;
+            this.includeUpperAndLowerCaseCheckBox.Text = "Include upper- and lower-case";
+            this.includeUpperAndLowerCaseCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // resultsListBox
+            // 
+            this.resultsListBox.FormattingEnabled = true;
+            this.resultsListBox.Location = new System.Drawing.Point(12, 179);
+            this.resultsListBox.Name = "resultsListBox";
+            this.resultsListBox.Size = new System.Drawing.Size(259, 186);
+            this.resultsListBox.TabIndex = 4;
+            this.resultsListBox.SelectedIndexChanged += new System.EventHandler(this.resultsListBox_SelectedIndexChanged);
+            // 
+            // generateOnePassRadioButton
+            // 
+            this.generateOnePassRadioButton.AutoSize = true;
+            this.generateOnePassRadioButton.Location = new System.Drawing.Point(6, 19);
+            this.generateOnePassRadioButton.Name = "generateOnePassRadioButton";
+            this.generateOnePassRadioButton.Size = new System.Drawing.Size(138, 17);
+            this.generateOnePassRadioButton.TabIndex = 5;
+            this.generateOnePassRadioButton.TabStop = true;
+            this.generateOnePassRadioButton.Text = "Generate one password";
+            this.generateOnePassRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // generateFivePasswordsRadioButton
+            // 
+            this.generateFivePasswordsRadioButton.AutoSize = true;
+            this.generateFivePasswordsRadioButton.Location = new System.Drawing.Point(6, 42);
+            this.generateFivePasswordsRadioButton.Name = "generateFivePasswordsRadioButton";
+            this.generateFivePasswordsRadioButton.Size = new System.Drawing.Size(142, 17);
+            this.generateFivePasswordsRadioButton.TabIndex = 6;
+            this.generateFivePasswordsRadioButton.TabStop = true;
+            this.generateFivePasswordsRadioButton.Text = "Generate five passwords";
+            this.generateFivePasswordsRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(13, 383);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(75, 23);
+            this.generateButton.TabIndex = 7;
+            this.generateButton.Text = "Generate";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(196, 383);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 9;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // generateRadioButtonContainer
+            // 
+            this.generateRadioButtonContainer.Controls.Add(this.generateOnePassRadioButton);
+            this.generateRadioButtonContainer.Controls.Add(this.generateFivePasswordsRadioButton);
+            this.generateRadioButtonContainer.Location = new System.Drawing.Point(13, 83);
+            this.generateRadioButtonContainer.Name = "generateRadioButtonContainer";
+            this.generateRadioButtonContainer.Size = new System.Drawing.Size(228, 72);
+            this.generateRadioButtonContainer.TabIndex = 10;
+            this.generateRadioButtonContainer.TabStop = false;
+            // 
+            // copyButton
+            // 
+            this.copyButton.Location = new System.Drawing.Point(104, 383);
+            this.copyButton.Name = "copyButton";
+            this.copyButton.Size = new System.Drawing.Size(75, 23);
+            this.copyButton.TabIndex = 8;
+            this.copyButton.Text = "Copy";
+            this.copyButton.UseVisualStyleBackColor = true;
+            // 
+            // passGenMainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(283, 414);
+            this.Controls.Add(this.generateRadioButtonContainer);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.copyButton);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.resultsListBox);
+            this.Controls.Add(this.includeUpperAndLowerCaseCheckBox);
+            this.Controls.Add(this.includeSpecialCharactersCheckBox);
+            this.Controls.Add(this.includeNumbersCheckBox);
+            this.Name = "passGenMainWindow";
+            this.Text = "Password Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.generateRadioButtonContainer.ResumeLayout(false);
+            this.generateRadioButtonContainer.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.CheckBox includeNumbersCheckBox;
+        private System.Windows.Forms.CheckBox includeSpecialCharactersCheckBox;
+        private System.Windows.Forms.CheckBox includeUpperAndLowerCaseCheckBox;
+        private System.Windows.Forms.ListBox resultsListBox;
+        private System.Windows.Forms.RadioButton generateOnePassRadioButton;
+        private System.Windows.Forms.RadioButton generateFivePasswordsRadioButton;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.GroupBox generateRadioButtonContainer;
+        private System.Windows.Forms.Button copyButton;
     }
 }
 
--- CST117 Programming Project 2/Form1.cs
+++ CST117 Programming Project 2/Form1.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿// Chad Weirick
+// CST-117
+// Grand Canyon Univeristy
+// Dr. J's class!
+//
+// Programming Project 2:
+// Requirements : Write a Windows Forms program that uses radio buttons, check boxes and list boxes.  
+//
+// Program goal: create a password generator.
+// Check boxes determine what types of characters can be added
+// radio buttons determine whether one or five passwords are created
+// the list box is used for output
+// in a bonus twist, I experimented with disabling buttons in an appropriate way
+// I also used selected items from the list as well as a system clipboard copy
+
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +26,172 @@ using System.Windows.Forms;
 
 namespace CST117_Programming_Project_2
 {
-    public partial class Form1 : Form
+    public partial class passGenMainWindow : Form
     {
-        public Form1()
+
+        public passGenMainWindow()
         {
             InitializeComponent();
+            copyButton.Enabled = false;
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            generateOnePassRadioButton.Checked = true;
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+
+            // create a list to handle the possible chars to be selected
+            List<char> possibleChars = new List<char>();
+            possibleChars.Add('a');
+            possibleChars.Add('b');
+            possibleChars.Add('c');
+            possibleChars.Add('d');
+            possibleChars.Add('e');
+            possibleChars.Add('f');
+            possibleChars.Add('g');
+            possibleChars.Add('h');
+            possibleChars.Add('i');
+            possibleChars.Add('j');
+            possibleChars.Add('k');
+            possibleChars.Add('l');
+            possibleChars.Add('m');
+            possibleChars.Add('n');
+            possibleChars.Add('o');
+            possibleChars.Add('p');
+            possibleChars.Add('q');
+            possibleChars.Add('r');
+            possibleChars.Add('s');
+            possibleChars.Add('t');
+            possibleChars.Add('u');
+            possibleChars.Add('v');
+            possibleChars.Add('w');
+            possibleChars.Add('x');
+            possibleChars.Add('y');
+            possibleChars.Add('z');
+      
+            // upper and lower case selected?
+            if (includeUpperAndLowerCaseCheckBox.Checked == true)
+            {
+                possibleChars.Add('A');
+                possibleChars.Add('B');
+                possibleChars.Add('C');
+                possibleChars.Add('D');
+                possibleChars.Add('E');
+                possibleChars.Add('F');
+                possibleChars.Add('G');
+                possibleChars.Add('H');
+                possibleChars.Add('I');
+                possibleChars.Add('J');
+                possibleChars.Add('K');
+                possibleChars.Add('L');
+                possibleChars.Add('M');
+                possibleChars.Add('N');
+                possibleChars.Add('O');
+                possibleChars.Add('P');
+                possibleChars.Add('Q');
+                possibleChars.Add('R');
+                possibleChars.Add('S');
+                possibleChars.Add('T');
+                possibleChars.Add('U');
+                possibleChars.Add('V');
+                possibleChars.Add('W');
+                possibleChars.Add('X');
+                possibleChars.Add('Y');
+                possibleChars.Add('Z');
+            }
+
+            // see if we need to add numbers to our list
+            if (includeNumbersCheckBox.Checked == true)
+            {
+                possibleChars.Add('0');
+                possibleChars.Add('1');
+                possibleChars.Add('2');
+                possibleChars.Add('3');
+                possibleChars.Add('4');
+                possibleChars.Add('5');
+                possibleChars.Add('6');
+                possibleChars.Add('7');
+                possibleChars.Add('8');
+                possibleChars.Add('9');
+
+            }
+
+            // see if we need to add special characters to our list
+            if (includeSpecialCharactersCheckBox.Checked == true)
+            {
+                possibleChars.Add('!');
+                possibleChars.Add('@');
+                possibleChars.Add('#');
+                possibleChars.Add('$');
+                possibleChars.Add('%');
+                possibleChars.Add('^');
+                possibleChars.Add('&');
+                possibleChars.Add('*');
+                possibleChars.Add('(');
+                possibleChars.Add(')');
+                possibleChars.Add('~');
+                possibleChars.Add('`');
+                possibleChars.Add('-');
+                possibleChars.Add('_');
+                possibleChars.Add('[');
+                possibleChars.Add(']');
+                possibleChars.Add(';');
+                possibleChars.Add('=');
+                possibleChars.Add('+');
+            }
+
+            int maxCounter = 0;
+            if (generateOnePassRadioButton.Checked == true)
+            {
+                maxCounter = 1;
+            }
+            if (generateFivePasswordsRadioButton.Checked == true)
+            {
+                maxCounter = 5;
+            }
+
+            // prep a random number generator and int r to be used to scan the list
+            Random rnd = new Random();
+            int r = 0;
+            // for efficiency use int totalChars to reduce recursive calls on possibleChars.Count
+            int totalChars = possibleChars.Count;
+
+            String thisPass = "";
+            for(int x = 0; x < maxCounter; x++)
+            {
+                thisPass = "";
+                for(int y = 0; y < 15; y++)
+                {
+                    r = rnd.Next(totalChars);
+                    thisPass = thisPass + possibleChars[r].ToString();
+                }
+                resultsListBox.Items.Add(thisPass);
+            }
+
+            //reset radio buttons for next use
+            generateOnePassRadioButton.Checked = true;
+            generateFivePasswordsRadioButton.Checked = false;
+
+        }
+
+        private void resultsListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            copyButton.Enabled = true;
+            Clipboard.SetText(resultsListBox.GetItemText(resultsListBox.SelectedItem));
+        }
+
     }
 }
